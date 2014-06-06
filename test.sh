9ca919c98abe046138a7c7f82b92a7a32ca6de20
Jeff Pace <jeugenepace@gmail.com>
2014-06-05 21:40:08 -0400

    Began to add test fixture.

--- test.sh
+++ test.sh
@@ -3,13 +3,15 @@
 # fetched from github.com/jpace/svnx, then the .git directory removed
 FROM_DIR=/tmp/svnx
 
-TRESYNC=/proj/org/incava/tresync/backup.rb
+TRESYNC=/proj/org/incava/tresync/bin/tresync.rb
 
 TEST_DIR=/tmp/tresync/testing
+
 FULL_DIR=$TEST_DIR/svnx
 INCR_DIR=$TEST_DIR.1/svnx
-CHANGED_COPIED_FILE=/tmp/svnx/README.md
-CHANGED_IGNORED_FILE=/tmp/svnx/test/integration/tc.rb
+
+CHANGED_COPIED_FILE=$FROM_DIR/README.md
+CHANGED_IGNORED_FILE=$FROM_DIR/test/integration/tc.rb
 
 rm -rf $TEST_DIR
 mkdir --parents $TEST_DIR
@@ -69,3 +71,25 @@ echo $INCR_DIR > $EXPECTED_CHANGED_INCR_CMP_FILE
 echo "$INCR_DIR/README.md" >> $EXPECTED_CHANGED_INCR_CMP_FILE
 
 cmp $EXPECTED_CHANGED_INCR_CMP_FILE $ACTUAL_CHANGED_INCR_CMP_FILE || echo "changed incremental backup failed"
+
+# now do the rsync business ...
+
+RSYNC_TGT_DIR=$TEST_DIR/rsynctgt/svnx
+
+rm -rf $RSYNC_TGT_DIR
+mkdir -p $RSYNC_TGT_DIR
+
+rsync -ra $FULL_DIR/ $RSYNC_TGT_DIR
+
+EXPECTED_FULL_CMP_FILE=$TEST_DIR/expected_full_cmp.txt
+ACTUAL_FULL_CMP_FILE=$TEST_DIR/actual_full_cmp.txt
+
+echo "Only in /tmp/svnx: pkg" > $EXPECTED_FULL_CMP_FILE
+echo "diff -r /tmp/svnx/README.md /tmp/tresync/testing/rsynctgt/svnx/README.md" >> $EXPECTED_FULL_CMP_FILE
+echo "42d41" >> $EXPECTED_FULL_CMP_FILE
+echo "< a new line" >> $EXPECTED_FULL_CMP_FILE
+echo "Only in /tmp/svnx/test: integration" >> $EXPECTED_FULL_CMP_FILE
+
+diff -r $FROM_DIR $RSYNC_TGT_DIR > $ACTUAL_FULL_CMP_FILE
+
+cmp $EXPECTED_FULL_CMP_FILE $ACTUAL_FULL_CMP_FILE || echo "rsync full sync failed"
